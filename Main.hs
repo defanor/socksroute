02fece1e2db2c3bc8d6e58b11fc7afd216b79e11
defanor <defanor@thunix.net>
2024-07-29 12:09:47 +0300

    Split TLS records on SNI
    Mitigating DPI-based censorship.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import Network.Socks5.Lowlevel
@@ -6,12 +5,13 @@ import Network.Socks5.Types
 import Network.Socks5
 import Network.Socket
 import qualified Network.Socket.ByteString as NSB
-import qualified Network.Simple.TCP as NST
 import Control.Concurrent.Async
 import Control.Monad
-import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString as BS
 import Control.Exception as E
-
+import Control.Concurrent
+import Control.Applicative
+import Data.Char
 
 data Rule = Socks SocksConf
           | Plain
@@ -19,19 +19,14 @@ data Rule = Socks SocksConf
 domainRules :: SocksHostAddress -> Rule
 domainRules (SocksAddrDomainName s)
   | BS.isSuffixOf ".onion" s = tor
-  | BS.isSuffixOf "archive.org" s = tor
-  | BS.isSuffixOf "trulyergonomic.com" s = tor
-  | BS.isSuffixOf "googlevideo.com" s = Plain
-  | BS.isInfixOf "google" s = tor
-  | BS.isSuffixOf "gstatic.com" s = tor
-  | BS.isSuffixOf "rutracker.org" s = tor
-  | BS.isSuffixOf "thepiratebay.se" s = tor
-  | BS.isSuffixOf "wordpress.com" s = tor
   | otherwise = Plain
   where
-    tor = Socks $ defaultSocksConf "localhost" $ fromIntegral 9050
+    tor = Socks $ defaultSocksConf
+      (SockAddrInet (fromIntegral 9050) (tupleToHostAddress (127, 0, 0, 1)))
 domainRules _ = Plain
 
+splitStrings :: [BS.ByteString]
+splitStrings = ["googlevideo.com"]
 
 toSockAddr :: PortNumber -> SocksHostAddress -> IO (Maybe SockAddr)
 toSockAddr pn (SocksAddrIPV4 ha4) = return . Just $ SockAddrInet pn ha4
@@ -41,16 +36,31 @@ toSockAddr pn (SocksAddrDomainName dn) = do
   -- a family. Easy to add, but I don't need (and don't have) IPv6.
   ai <- getAddrInfo (Just $ defaultHints { addrSocketType = Stream,
                                            addrFamily = AF_INET })
-        (Just $ BS.unpack dn) (Just $ show pn)
+        (Just $ map (chr . fromIntegral) $ BS.unpack dn) (Just $ show pn)
   case ai of
     -- Would be better to return the whole list, and try them in
     -- order; ToDo, if will try to make it all nice someday.
     (x:_) -> return . Just $ addrAddress x
     _ -> return Nothing
 
+serveTCP :: (Socket -> SockAddr -> IO ()) -> IO ()
+serveTCP f = do
+  let addr = SockAddrInet (fromIntegral 1080) (tupleToHostAddress (127, 0, 0, 1))
+  E.bracket
+    (socket AF_INET Stream defaultProtocol)
+    close
+    $ \sock -> do
+    setSocketOption sock ReuseAddr 1
+    setSocketOption sock NoDelay 1
+    bind sock addr
+    withFdSocket sock setCloseOnExecIfNeeded
+    listen sock 128
+    forever $ do
+      (conn, peer) <- accept sock
+      forkIO (f conn peer `finally` close conn)
 
 main :: IO ()
-main = NST.serve (NST.Host "127.0.0.1") "1080" $ \(s, saddr) -> do
+main = serveTCP $ \s saddr -> do
   (SocksRequest cmd sdAddr sdPort) <- socksListen s
   case cmd of
     SocksCommandConnect -> do
@@ -61,6 +71,7 @@ main = NST.serve (NST.Host "127.0.0.1") "1080" $ \(s, saddr) -> do
             Nothing -> putStrLn "Failed to get sockaddr"
             Just sa -> do
               s' <- socket AF_INET Stream defaultProtocol
+              setSocketOption s' NoDelay 1
               connect s' sa
               sendSerialized s $ SocksResponse SocksReplySuccess sdAddr sdPort
               proxy s s'
@@ -73,20 +84,50 @@ main = NST.serve (NST.Host "127.0.0.1") "1080" $ \(s, saddr) -> do
     proxy :: Socket -> Socket -> IO ()
     proxy f t = do
       -- Might be better to use `poll` here, not separate threads.
-      c2s <- async $ relay f t
-      s2c <- async $ relay t f
+      c2s <- async $ relay f t splitStrings
+      s2c <- async $ relay t f []
       (a, r) <- waitAnyCatchCancel [c2s, s2c]
       case r of
         Left e -> putStrLn $ "Exception: " ++ show e
         Right v -> return ()
-      tc <- isConnected t
-      when tc $ close t
-      fc <- isConnected f
-      when fc $ close f
+      close t
+      close f
 
-    relay :: Socket -> Socket -> IO ()
-    relay f t = do
+    tryToBreak :: BS.ByteString -> BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
+    tryToBreak whole sub = case BS.breakSubstring sub whole of
+      (x, "") -> Nothing
+      (x, y') -> case BS.splitAt (BS.length sub `div` 2) y' of
+        (y1, y2) -> pure (BS.append x y1, y2)
+
+    -- A very crude TLS record splitting
+    tlsTryToBreak whole sub =
+      let (s1, s2) = BS.splitAt 5 whole
+      in case BS.unpack s1 of
+        [22, v1, v2, l1, l2] ->
+          let len = fromIntegral l1 * 0x100 + fromIntegral l2
+          in do
+            (b1, b2) <- tryToBreak s2 sub
+            let l1 = BS.length b1
+                l2 = BS.length b2
+                mkHeader l = BS.pack [22, v1, v2,
+                                      fromIntegral (l `div` 0x100),
+                                      fromIntegral (l `mod` 0x100)]
+            pure (BS.append (mkHeader l1) b1, BS.append (mkHeader l2) b2)
+        _ -> tryToBreak whole sub
+
+    relay :: Socket -> Socket -> [BS.ByteString] -> IO ()
+    relay f t ss = do
       v <- NSB.recv f 4096
       case v of
         "" -> return ()
-        v' -> NSB.sendAll t v' >> relay f t
+        v' -> do
+          case foldl (<|>) Nothing (map (tlsTryToBreak v') splitStrings) of
+            Nothing -> NSB.sendAll t v'
+            Just (v1, v2) ->
+              -- Might be useful to send those 200 ms or so apart, to
+              -- avoid joining of the pockets. But the observed DPI
+              -- does not seem to be confused by split TCP packets
+              -- anyway, so depending more on the split TLS records
+              -- here.
+              NSB.sendAll t v1 >> NSB.sendAll t v2
+          relay f t ss
--- README.md
+++ README.md
@@ -11,24 +11,34 @@ set
 
 Currently there's not much of error reporting, exception handling, or
 even logging, and probably things like Privoxy can do what it does;
-this one is rather ad hoc, written after me being unable to quickly
-find such a program. But it works fine.
+this one is rather ad hoc, initially written after me being unable to
+quickly find such a program.
+
+For Firefox, extensions like FoxyProxy handle this task as well.
+
+As of 2024, due to the increased DPI-assisted SNI-based censorship,
+splitting of TLS records on provided substrings is added. Yet again,
+there are other projects doing the same thing (see "Green Tunnel",
+"zapret"), but I have not found anything simple and otherwise
+satisfactory, being just a SOCKS5 server, without a GUI, and not doing
+much else.
 
 
-## Installation ##
+## Running and installation ##
 
-To build the program in a cabal sandbox and install a systemd service,
-`make install`. To uninstall it, `make uninstall`.
+Simply `cabal run` to run it manually, or `cabal install`, possibly
+copy the executable into a more appropriate location, install a
+systemd service (an example one is provided).
 
 
 ## Configuration ##
 
-Configuration is currently getting done in the code, which is short;
-particularly in the `domainRules` function.
+Configuration is currently done in the code, which is short;
+particularly in `domainRules` and `splitStrings`.
 
-By default, it listens on `127.0.0.1:1080`, and routes .onion and
-google resources (as well as a couple of others) through Tor SOCKS
-with its default configuration (`localhost:9050`).
+By default, it listens on `127.0.0.1:1080`, routes .onion resources
+through Tor SOCKS with its default configuration (`localhost:9050`),
+splits TLS records for googlevideo.com.
 
 Something like "Remote DNS" should be set in a client program if the
 rules rely on domain names.
